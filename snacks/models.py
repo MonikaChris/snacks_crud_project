893aa273c2682d79c4a87dcd1faa23715c9d77c9
Monika <monidavies@gmail.com>
2023-01-07 14:19:39 -0800

    Setup basic Django project, app, snack model

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
+class Snack(models.Model):
+    tite = models.CharField(max_length=100)
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    description = models.TextField()
+
