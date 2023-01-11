d5b81a725f35e953a80c28aab19eb8de1219f641
Monika <monidavies@gmail.com>
2023-01-11 14:22:52 -0800

    Adds custom user model

--- snacks_crud_project/settings.py
+++ snacks_crud_project/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'snacks'
+    'snacks',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'account.Account'
